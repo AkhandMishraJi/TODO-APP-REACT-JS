098b06c2f1437a742f20363a7fa486248dd5edda
CODEWITHAKHAND <kanahmishra286@gmail.com>
2024-06-29 20:20:07 +0530

    GITHUB

--- todo-app-react-js/src/store.js
+++ todo-app-react-js/src/store.js
@@ -1,1 +1,7 @@
-import { createStore } from 'redux'
+import { createStore , combineReducers} from 'redux'
+ import { todoReducer } from "./components/reducers/todoReducer";
+
+ const reducers = combineReducers({todo : todoReducer})
+ const store = createStore(reducers)
+
+ export default store
