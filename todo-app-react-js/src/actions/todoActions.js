34b7ae1cfb76a23502fcb7fcaee1a9dc0c4b5bc7
CODEWITHAKHAND <kanahmishra286@gmail.com>
2024-06-29 22:25:09 +0530

    hello

--- todo-app-react-js/src/actions/todoActions.js
+++ todo-app-react-js/src/actions/todoActions.js
@@ -1,3 +1,4 @@
+import {  } from "../constants/actions";
 export const todoFinished = (todo , isFinished) => ({type : "finish_todo" , payload : {todo , isFinished : isFinished}})
 export const addTodo = (inputText) => ({type : "add_todo" , payload : {todoText :  inputText}})
 export const deleteTodo = (todo) => ({type : "delete_todo" , payload : {todo}})
